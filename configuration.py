f4888884af1827d704b00e7358cae0263fac6886
Varuna S <82440654+Varuna-S@users.noreply.github.com>
2021-04-13 14:10:28 +0000

    First commit

---
+++ configuration.py
@@ -0,0 +1,11 @@
+c3d_model_weights = './trained_models/c3d_sports1m.h5'
+classifier_model_weigts = './trained_models/weights_L1L2.mat'
+classifier_model_json = './trained_models/model.json'
+
+input_folder  = './input'
+output_folder = './output'
+
+AllClassPath = './out'
+trainedModelOut = './trained_models'
+
+sample_video_path = './input/*.mp4'
